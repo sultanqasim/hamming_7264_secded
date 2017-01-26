02318ac8efb7a6c7d180dd65b7e13e125723c576
Sultan Qasim Khan <sultanqasim@gmail.com>
2017-01-26 01:39:08 -0500

    Add descriptions in header

--- fec_secded7264.h
+++ fec_secded7264.h
@@ -1,10 +1,34 @@
 #ifndef FEC7264_H
 #define FEC7264_H
 
-unsigned int fec_secded7264_encode(unsigned int _dec_msg_len,
-                                   const unsigned char *_msg_dec,
-                                   unsigned char *_msg_enc);
+/* fec_secded7264_encode
+ *
+ * Inputs:
+ * dec_msg_len  size of original data
+ * msg_dec      original message data
+ *
+ * Outputs:
+ * msg_enc      encoded message data gets put here
+ *
+ * Returns:
+ * Size of encoded message in bytes
+ */
+unsigned int fec_secded7264_encode(unsigned int dec_msg_len,
+                                   const unsigned char *msg_dec,
+                                   unsigned char *msg_enc);
 
+/* fec_secded7264_decode
+ *
+ * Inputs:
+ * dec_msg_len  size of data after decoding
+ * msg_enc      encoded message data
+ *
+ * Outputs:
+ * msg_dec      decoded original mesasage data gets put here
+ *
+ * Returns:
+ * Number of unrecoverable errors encountered in decoding
+ */
 unsigned int fec_secded7264_decode(unsigned int dec_msg_len,
                                    const unsigned char *msg_enc,
                                    unsigned char *msg_dec);
