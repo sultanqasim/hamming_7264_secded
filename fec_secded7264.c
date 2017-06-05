sultanqasim/hamming_7264_secded

commit 91621d959d9366daad7d199c84cefe062f66f415

Change decode length parameter

fec_secded7264.c
@@ -295,11 +295,11 @@ unsigned int fec_secded7264_encode(unsigned int _dec_msg_len,
 
 // decode block of data using SEC-DEC (72,64) decoder
 //
-//  _dec_msg_len    :   decoded message length (number of bytes)
+//  _enc_msg_len    :   encoded message length (number of bytes)
 //  _msg_enc        :   encoded message [size: 1 x 2*_dec_msg_len]
 //  _msg_dec        :   decoded message [size: 1 x _dec_msg_len]
 //
-unsigned int fec_secded7264_decode(unsigned int _dec_msg_len,
+unsigned int fec_secded7264_decode(unsigned int _enc_msg_len,
                                    const unsigned char *_msg_enc,
                                    unsigned char *_msg_dec)
 {
@@ -308,17 +308,16 @@ unsigned int fec_secded7264_decode(unsigned int _dec_msg_len,
     unsigned int num_errors = 0;
     unsigned int syndrome_flag;
 
-    // determine remainder of input length / 8
-    unsigned int r = _dec_msg_len % 8;
+    // determine remainder of input length
+    unsigned int r = _enc_msg_len % 9;
 
-    for (i=0; i<_dec_msg_len-r; i+=8) {
+    for (j=0; j<_enc_msg_len-r; j+=9) {
         // decode nine input bytes
         syndrome_flag = fec_secded7264_decode_symbol(&_msg_enc[j], &_msg_dec[i]);
         if (syndrome_flag == 2) num_errors++;
 
-        j += 9;
+        i += 8;
     }
-
 
     // if input length isn't divisible by 8, decode last several bytes
     if (r) {
@@ -338,8 +337,8 @@ unsigned int fec_secded7264_decode(unsigned int _dec_msg_len,
         for (n=0; n<r; n++)
             _msg_dec[i+n] = c[n];
 
-        i += r;
-        j += r+1;
+        i += r - 1;
+        j += r;
     }
 
     return num_errors;

fec_secded7264.h
@@ -20,7 +20,7 @@ unsigned int fec_secded7264_encode(unsigned int dec_msg_len,
 /* fec_secded7264_decode
  *
  * Inputs:
- * dec_msg_len  size of data after decoding
+ * dec_msg_len  size of encoded data
  * msg_enc      encoded message data
  *
  * Outputs:
@@ -29,7 +29,7 @@ unsigned int fec_secded7264_encode(unsigned int dec_msg_len,
  * Returns:
  * Number of unrecoverable errors encountered in decoding
  */
-unsigned int fec_secded7264_decode(unsigned int dec_msg_len,
+unsigned int fec_secded7264_decode(unsigned int enc_msg_len,
                                    const unsigned char *msg_enc,
                                    unsigned char *msg_dec);
 

commit 61d9d3d69345e30c386ba4aa3b0de2b08b0c5c72

fix buffer over-read in fec_secded7264_decode()
As described in #1, there is a buffer over-read in the function
fec_secded7264_decode() when copying the remaining bytes.

This bug was introduced by the commit 91621d9, which changed the
meaning of the first argument from the length of the original message
to encoded one.  If we get an original message length as it used to
be, we need additional byte for parity. However, we already know how
long the remaining bytes are, we can just copy it.  We must not copy
another one byte which leads us to illegal memory access.

Valgrind says:

$ valgrind ./fec_test
==11769== Memcheck, a memory error detector
==11769== Copyright (C) 2002-2015, and GNU GPL'd, by Julian Seward et al.
==11769== Using Valgrind-3.12.0 and LibVEX; rerun with -h for copyright info
==11769== Command: ./fec_test
==11769==
==11769== Invalid read of size 1
==11769==    at 0x10989B: fec_secded7264_decode (fec_secded7264.c:331)
==11769==    by 0x1087C9: main (main.c:50)
==11769==  Address 0x51d7069 is 0 bytes after a block of size 41 alloc'd
==11769==    at 0x4C2BBAF: malloc (vg_replace_malloc.c:299)
==11769==    by 0x10877D: main (main.c:40)
==11769==
Orig len 36 to enc len 41 to dec len 36 with 1 errors.
original: this is some text that is test data
corrupt:  this ms some0text 5hat is test data
decoded:  this is some text 5hat is test data
==11769==
==11769== HEAP SUMMARY:
==11769==     in use at exit: 0 bytes in 0 blocks
==11769==   total heap usage: 2 allocs, 2 frees, 1,065 bytes allocated
==11769==
==11769== All heap blocks were freed -- no leaks are possible
==11769==
==11769== For counts of detected and suppressed errors, rerun with: -v
==11769== ERROR SUMMARY: 1 errors from 1 contexts (suppressed: 0 from 0)

This fixes #1.

fec_secded7264.c
@@ -326,8 +326,8 @@ unsigned int fec_secded7264_decode(unsigned int _enc_msg_len,
         unsigned char c[8] = {0,0,0,0,0,0,0,0};     // decoded message
 
         unsigned int n;
-        // output length is input + 1 (parity byte)
-        for (n=0; n<r+1; n++)
+        // copy the remaining bytes to local buffer v
+        for (n=0; n<r; n++)
             v[n] = _msg_enc[j+n];
 
         // decode symbol